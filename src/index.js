699952071adc2792fc2cf9ac737f79f346cd37d8
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-08-14 15:51:26 +0100

    upgrade n-auto-logger  🐿 v2.8.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+export { default as logger } from '@financial-times/n-auto-logger';
+
 export { monitorService, monitorModule } from './action';
 export { default as monitor } from './operation';
 export { default as setupMonitor } from './setup';
