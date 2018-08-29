787ce916f85163eab9b23b7bc8c6c7e69eefd5db
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-08-29 12:56:10 +0100

    add logger setup  🐿 v2.8.0

--- src/setup.js
+++ src/setup.js
@@ -1,11 +1,15 @@
 import { initAutoMetrics } from '@financial-times/n-auto-metrics';
 import {
+	setupLoggerInstance,
 	requestIdMiddleware,
 	enhancedRender,
 } from '@financial-times/n-auto-logger';
 
-const setupMonitor = ({ app, metrics }) => {
+const setupMonitor = ({ app, metrics, logger }) => {
 	initAutoMetrics(metrics);
+	if (logger) {
+		setupLoggerInstance(logger);
+	}
 	app.use(/^\/(?!_{2}).*$/, [requestIdMiddleware, enhancedRender]);
 };
 
